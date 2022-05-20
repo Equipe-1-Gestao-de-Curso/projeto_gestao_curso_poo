d033bbbb7ec208271c6c34760519e3cb70db6355
Gabriel Araújo Gonçalves <1221300455@acd.grupotiradentes.com>
2022-05-19 21:30:15 -0300

    First commit

---
+++ src/main/java/com/unit/crud_curso/CrudCursoApplication.java
@@ -0,0 +1,13 @@
+package com.unit.crud_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unit/crud_curso/CrudCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit.crud_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
