ce04a1910d810a2fc75bd6f6853e4402fb1abd52
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2022-06-10 22:40:26 -0300

    conclusão do código

--- src/main/java/com/unit/crud_curso/controller/QuadroAvisosController.java
+++ src/main/java/com/unit/crud_curso/controller/QuadroAvisosController.java
@@ -1,87 +1,108 @@
 package com.unit.crud_curso.controller;
+
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import com.unit.crud_curso.repository.QuadroAvisosRepo;
 import com.unit.crud_curso.model.QuadroAvisos;
-
-import com.unit.crud_curso.repository.UserRepo;
 import com.unit.crud_curso.model.Usuarios;
-@RestController
-@RequestMapping(value="/avisos")
-public class QuadroAvisosController {
+import com.unit.crud_curso.repository.QuadroAvisosRepo;
+import com.unit.crud_curso.repository.UserRepo;
 
+@RestController
+@RequestMapping(value = "/avisos")
+public class QuadroAvisosController {
+  
   @Autowired
   private QuadroAvisosRepo quadroAvisosRepo;
+
+  @Autowired
   private UserRepo userRepo;
   
-  // @GetMapping
-  // public List<QuadroAvisos> findAll(@RequestBody Usuarios usuarioLogado){
-  //   if(usuarioLogado.getPerfilAcesso() != "Adm"){
-  //     return quadroAvisosRepo.findAll();
-
-  //   }
-  //   else{
-  //     return ;
-  //   }
-  // }
-
-  @PostMapping
-  public QuadroAvisos save(@RequestBody QuadroAvisos novoQuadroAvisos){
-    Usuarios userVazio = new Usuarios();
-    Usuarios usuarioBD = userRepo.findById(novoQuadroAvisos.getAutor()).orElse(userVazio);
-    if(usuarioBD.getPerfilAcesso() == "Coord" || usuarioBD.getPerfilAcesso() == "Prof"){
-      String timeStamp = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
-      novoQuadroAvisos.setDataCriacao(timeStamp);
-      novoQuadroAvisos.setDataModificacao(timeStamp);
-      quadroAvisosRepo.save(novoQuadroAvisos);
-      return novoQuadroAvisos;
+  @GetMapping(value = "/{id}") List<QuadroAvisos> verQuadrosAvisos(@PathVariable Long id){
+    Usuarios usuarioDB = userRepo.findById(id).get();
+    String perfil = usuarioDB.getPerfilAcesso();
+    if(!perfil.equals("Adm")){
+      return quadroAvisosRepo.findAll();
     }
     else{
-      QuadroAvisos quadroVazio = new QuadroAvisos();
-      return quadroVazio;
+      ArrayList<QuadroAvisos> avisoVazio = new ArrayList<>();
+      return avisoVazio;
     }
-   
   }
 
-  @PutMapping
-  public Usuarios update(@RequestBody Usuarios usuarioAtualizar){
-    Usuarios usuarioBD = userRepo.findById(usuarioAtualizar.getId()).get();
-    
-    if(usuarioBD.getName() != usuarioAtualizar.getName() && usuarioAtualizar.getName() != null){
-      usuarioBD.setName(usuarioAtualizar.getName());
+  @PostMapping(value = "/{id}") String cadastrarQuadrosAvisos(@RequestBody QuadroAvisos entity, @PathVariable Long id){
+    Usuarios usuarioDB = userRepo.findById(id).get();
+    String perfil = usuarioDB.getPerfilAcesso();
+    if(!perfil.equals("Adm") && !perfil.equals("Aluno")){
+      Date data = new Date();
+      SimpleDateFormat formatador = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+      String dataAtual = formatador.format(data);
+      entity.setDataCriacao(dataAtual);
+      entity.setDataModificacao(dataAtual);
+      quadroAvisosRepo.save(entity);
+      return "QUADRO DE AVISO CRIADO!!!!";
     }
-    if(usuarioBD.getEmail() != usuarioAtualizar.getEmail() && usuarioAtualizar.getEmail() != null){
-      usuarioBD.setEmail(usuarioAtualizar.getEmail());
-    }
-    if(usuarioBD.getCpf() != usuarioAtualizar.getCpf() && usuarioAtualizar.getCpf() != null){
-      usuarioBD.setCpf(usuarioAtualizar.getCpf());
-    }
-    if(usuarioBD.getPerfilAcesso() != usuarioAtualizar.getPerfilAcesso() && usuarioAtualizar.getPerfilAcesso() != null){
-      usuarioBD.setPerfilAcesso(usuarioAtualizar.getPerfilAcesso());
-    }
-    if(usuarioBD.getSenha() != usuarioAtualizar.getSenha() && usuarioAtualizar.getSenha() != null){
-      usuarioBD.setSenha(usuarioAtualizar.getSenha());
-    }
-   
-   
-    return userRepo.save(usuarioBD);
-  }
-
-  @DeleteMapping
-  public String delete(@RequestBody Usuarios usuarioDeletar){
-    userRepo.delete(usuarioDeletar);
-    return "usuario deletado";
-  }
+    return "VOCÊ NÃO TEM PERMISSÃO";
   
+  }
+
+  @PutMapping(value = "/{userId}={id}") String atualizarQuadroAvisos(@RequestBody QuadroAvisos entity, @PathVariable Long id, @PathVariable Long userId){
+    Usuarios usuarioDB = userRepo.findById(userId).get();
+    String perfil = usuarioDB.getPerfilAcesso();
+    if(!perfil.equals("Adm") && !perfil.equals("Aluno")){
+      QuadroAvisos quadroAvisosDB = quadroAvisosRepo.findById(id).get();
+      Date data = new Date();
+      SimpleDateFormat formatador = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+      String dataAtual = formatador.format(data);
+      quadroAvisosDB.setDataModificacao(dataAtual);
+
+      if(quadroAvisosDB.getTitulo() != entity.getTitulo() && entity.getTitulo() != null){
+        quadroAvisosDB.setTitulo(entity.getTitulo());
+      }
+      if(quadroAvisosDB.getPrioridade() != entity.getPrioridade() && entity.getPrioridade() != null){
+        quadroAvisosDB.setPrioridade(entity.getPrioridade());
+      }
+      if(quadroAvisosDB.getDescricao() != entity.getDescricao() && entity.getDescricao() != null){
+        quadroAvisosDB.setDescricao(entity.getDescricao());
+      }
+      if(quadroAvisosDB.getAutor() != entity.getAutor() && entity.getAutor() != null){
+        quadroAvisosDB.setAutor(entity.getAutor());
+      }
+      quadroAvisosRepo.save(quadroAvisosDB);
+      return "QUADRO DE AVISOS ATUALIZADO COM SUCESSO!!!";
+    }
+    return "VOCÊ NÃO POSSUI PERMISSÃO!!!!";
+
+  }
+
+  @DeleteMapping(value = "/delete/{userId}={id}")
+  public String delete(@PathVariable Long id, @PathVariable Long userId){
+    Usuarios usuarioDB = userRepo.findById(userId).get();
+    String perfil = usuarioDB.getPerfilAcesso();
+    if(!perfil.equals("Adm") && !perfil.equals("Aluno")){
+      QuadroAvisos avisos = quadroAvisosRepo.findById(id).orElse(null);
+      if(avisos == null){
+        return "QUADRO DE AVISOS NÃO EXISTE!!!!";
+      }
+      else{
+        quadroAvisosRepo.delete(avisos);
+        return "QUADRO DE AVISOS DELETADO COM SUCESSO!!!!";
+      }
+    }
+    return "VOCÊ NÃO TEM PERMISSÃO PARA DELETAR!!!!"; 
+  }
 }
--- src/main/java/com/unit/crud_curso/controller/UserController.java
+++ src/main/java/com/unit/crud_curso/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,11 @@ public class UserController {
   @Autowired
   private UserRepo userRepo;
   
+  @GetMapping(value = "/{id}")
+  public Usuarios findById(@PathVariable Long id){
+    return userRepo.findById(id).get();
+  }
+
   @GetMapping
   public List<Usuarios> findAll(){
     return userRepo.findAll();
